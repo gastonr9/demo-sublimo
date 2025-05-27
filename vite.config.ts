ad6f8be3be9c0b3bb842638fa61f75d30e27cb9c
Gaston Rodriguez <lpmgaston@gmail.com>
2025-05-26 23:52:01 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "",
+  base: "demo-sublimo",
   plugins: [react()],
   assetsInclude: ["**/*.glb"],
 });
