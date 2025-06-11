86b900d4eef210bf0edd09c2890cd2f4d08ed944
Gaston Rodriguez <lpmgaston@gmail.com>
2025-06-11 19:00:29 -0300

    Refactor code structure for improved readability and maintainability

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import Index from "./pages/index";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+
 const App = () => {
   return (
     <div className="fondo">
       <Index></Index>;
+      <Routes>
+        <Route path="/demo" element={<Home />} />
+      </Routes>
     </div>
   );
 };
